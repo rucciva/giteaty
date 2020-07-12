58839ade67dfae41924bd52430a1c88929b7c8b2
Rucciva <rucciva@gmail.com>
2020-07-12 16:12:01 +0700

    empty team now assert org membership only

--- pkg/caddyhandler/assert-org.go
+++ pkg/caddyhandler/assert-org.go
@@ -14,9 +14,9 @@ type orgConfig struct {
 	teams      map[string]bool
 }
 
-func (drt *Directive) assertOrgTeamMiddleware(next http.Handler) http.Handler {
+func (drt *Directive) assertOrgMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		err := drt.assertOrgTeam(r, drt.getOrg(r))
+		err := drt.assertOrg(r, drt.getOrg(r))
 		if err != nil {
 			setReturn(r.Context(), handlerReturn{i: 403, err: err})
 			return
@@ -33,6 +33,24 @@ func (drt *Directive) getOrg(r *http.Request) (name string) {
 	return
 }
 
+func (drt *Directive) assertOrg(req *http.Request, orgname string) (err error) {
+	if len(drt.org.teams) > 0 {
+		return drt.assertOrgTeam(req, orgname)
+	}
+
+	gcl := drt.newGiteaClient(req)
+	orgs, err := gcl.ListMyOrgs(gitea.ListOrgsOptions{})
+	if err != nil {
+		return errUnauthorized
+	}
+	for _, org := range orgs {
+		if strings.EqualFold(orgname, org.UserName) {
+			return
+		}
+	}
+	return errUnauthorized
+}
+
 func (drt *Directive) assertOrgTeam(req *http.Request, orgname string) (err error) {
 	gcl := drt.newGiteaClient(req)
 	teams, err := gcl.ListMyTeams(&gitea.ListTeamsOptions{})
@@ -40,10 +58,11 @@ func (drt *Directive) assertOrgTeam(req *http.Request, orgname string) (err erro
 		return errUnauthorized
 	}
 	for _, team := range teams {
-		if strings.EqualFold(team.Organization.UserName, orgname) {
-			if _, ok := drt.org.teams[strings.ToLower(team.Name)]; ok {
-				return
-			}
+		if !strings.EqualFold(team.Organization.UserName, orgname) {
+			continue
+		}
+		if _, ok := drt.org.teams[strings.ToLower(team.Name)]; ok {
+			return
 		}
 	}
 	return errUnauthorized
